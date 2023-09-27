0db0db4f6b87e85f992e18bc8843e4f8eb26d08f
Emil <emilwilliams@tuta.io>
2023-09-27 05:49:50 +0000

    cd project root

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd $(dirname "$(readlink -f "$0")")
 SUDO=${SUDO:-sudo}
 cc baked.c -o baked -std=gnu99 -O2 -Wall -Wextra -Wpedantic -pipe $CFLAGS
 $SUDO install -m 755 baked /usr/local/bin
