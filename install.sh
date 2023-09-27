bc2c675571f9fc1e001d69bb04d5afbc6545f1ca
Emil <emilwilliams@tuta.io>
2023-09-27 05:58:21 +0000

    More flexible build with TARGET

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cd $(dirname "$(readlink -f "$0")")
 SUDO=${SUDO:-sudo}
 cc baked.c -o baked -std=gnu99 -O2 -Wall -Wextra -Wpedantic -pipe $CFLAGS
-$SUDO install -m 755 baked /usr/local/bin
+$SUDO install -m 755 baked ${TARGET:-/usr/local/bin}
