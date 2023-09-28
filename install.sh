e7ab816787ce1d40008e8016926b3ddf80383efe
Emil <emilwilliams@tuta.io>
2023-09-28 18:33:29 +0000

    gnu89

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname "$(readlink -f "$0")")
 SUDO=${SUDO:-sudo}
-cc baked.c -o baked -std=gnu99 -O2 -Wall -Wextra -Wpedantic -pipe $CFLAGS
+cc baked.c -o baked -std=gnu89 -O2 -Wall -Wextra -Wpedantic -pipe $CFLAGS
 $SUDO install -m 755 baked ${TARGET:-/usr/local/bin}
