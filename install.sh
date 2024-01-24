f21841ad9944746d6109730afbe61a319f1ff5fe
Emil Williams <emilwilliams@tuta.io>
2024-01-24 14:00:46 +0000

    fixed shake eval yet again

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $(dirname "$(readlink -f "$0")")
-SUDO=${SUDO:-sudo}
+SUDO=${SUDO-sudo}
 chmod +x shake
 ./shake bake.c -s $@ && \
 $SUDO install -m 755 shake bake ${TARGET:-/usr/local/bin}
